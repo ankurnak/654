46d130d22daed51ac492f055baffaf931bc0b63a
ankurnak <112162607+ankurnak@users.noreply.github.com>
2023-06-26 20:26:09 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ExpressionCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculate = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.substract = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.expressionTextBox = new System.Windows.Forms.TextBox();
+            this.resultLabel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.img = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(39, 274);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 0;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(337, 274);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 1;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // substract
+            // 
+            this.substract.Location = new System.Drawing.Point(639, 274);
+            this.substract.Name = "substract";
+            this.substract.Size = new System.Drawing.Size(75, 23);
+            this.substract.TabIndex = 2;
+            this.substract.Text = "-";
+            this.substract.UseVisualStyleBackColor = true;
+            this.substract.Click += new System.EventHandler(this.substract_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(39, 356);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(75, 23);
+            this.multiply.TabIndex = 3;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // divide
+            // 
+            this.divide.Location = new System.Drawing.Point(337, 356);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(75, 23);
+            this.divide.TabIndex = 4;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(639, 356);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 5;
+            this.clear.Text = "clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // expressionTextBox
+            // 
+            this.expressionTextBox.Location = new System.Drawing.Point(188, 149);
+            this.expressionTextBox.Name = "expressionTextBox";
+            this.expressionTextBox.Size = new System.Drawing.Size(100, 22);
+            this.expressionTextBox.TabIndex = 6;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Location = new System.Drawing.Point(530, 149);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(100, 22);
+            this.resultLabel.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "expression";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(399, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "result";
+            // 
+            // img
+            // 
+            this.img.Location = new System.Drawing.Point(198, 50);
+            this.img.Name = "img";
+            this.img.Size = new System.Drawing.Size(75, 23);
+            this.img.TabIndex = 10;
+            this.img.Text = "img";
+            this.img.UseVisualStyleBackColor = true;
+            this.img.Click += new System.EventHandler(this.img_Click);
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(542, 50);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(75, 23);
+            this.txt.TabIndex = 11;
+            this.txt.Text = "txt";
+            this.txt.UseVisualStyleBackColor = true;
+            this.txt.Click += new System.EventHandler(this.txt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.img);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.expressionTextBox);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.substract);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.Calculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button substract;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TextBox expressionTextBox;
+        private System.Windows.Forms.TextBox resultLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button img;
+        private System.Windows.Forms.Button txt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace ExpressionCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetDesign();
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            string expression = expressionTextBox.Text;
+            try
+            {
+                double result = ExpressionCalculator.Calculate(expression);
+                resultLabel.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid expression: " + ex.Message);
+            }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            expressionTextBox.Text += "+";
+        }
+
+        private void substract_Click(object sender, EventArgs e)
+        {
+            expressionTextBox.Text += "-";
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            expressionTextBox.Text += "*";
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            expressionTextBox.Text += "/";
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            expressionTextBox.Text = "";
+            resultLabel.Text = "";
+        }
+
+        private void img_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string imagePath = openFileDialog.FileName;
+                    string expression = ReadExpressionFromImage(imagePath);
+                    expressionTextBox.Text = expression;
+
+                    try
+                    {
+                        double result = ExpressionCalculator.Calculate(expression);
+                        resultLabel.Text = result.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Invalid expression: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void txt_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text Files|*.txt";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    string expression = ReadExpressionFromFile(filePath);
+                    expressionTextBox.Text = expression;
+
+                    try
+                    {
+                        double result = ExpressionCalculator.Calculate(expression);
+                        resultLabel.Text = result.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Invalid expression: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private string ReadExpressionFromImage(string imagePath)
+        {
+            using (var engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default))
+            {
+                using (var image = new Bitmap(imagePath))
+                {
+                    using (var page = engine.Process(image, PageSegMode.Auto))
+                    {
+                        return page.GetText().Trim();
+                    }
+                }
+            }
+        }
+
+        private string ReadExpressionFromFile(string filePath)
+        {
+            return File.ReadAllText(filePath).Trim();
+        }
+
+        private void SetDesign()
+        {
+          
+            expressionTextBox.BackColor = Color.White;
+            expressionTextBox.Font = new Font("Arial", 12, FontStyle.Regular);
+
+            resultLabel.BackColor = Color.White;
+            resultLabel.Font = new Font("Arial", 12, FontStyle.Bold);
+
+        
+            Calculate.BackColor = Color.FromArgb(0, 123, 255); 
+            Calculate.ForeColor = Color.White;
+            Calculate.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            add.BackColor = Color.FromArgb(0, 123, 255); 
+            add.ForeColor = Color.White;
+            add.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            substract.BackColor = Color.FromArgb(0, 123, 255); 
+            substract.ForeColor = Color.White;
+            substract.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            multiply.BackColor = Color.FromArgb(0, 123, 255); 
+            multiply.ForeColor = Color.White;
+            multiply.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            divide.BackColor = Color.FromArgb(0, 123, 255); 
+            divide.ForeColor = Color.White;
+            divide.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            clear.BackColor = Color.FromArgb(255, 61, 61); 
+            clear.ForeColor = Color.White;
+            clear.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            img.BackColor = Color.FromArgb(76, 175, 80); 
+            img.ForeColor = Color.White;
+            img.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            txt.BackColor = Color.FromArgb(76, 175, 80); 
+            txt.ForeColor = Color.White;
+            txt.Font = new Font("Arial", 12, FontStyle.Bold);
+        }
+
+    }
+    public static class ExpressionCalculator
+    {
+        public static double Calculate(string expression)
+        {
+       
+            ValidateExpression(expression);
+
+            DataTable table = new DataTable();
+            table.Columns.Add("expression", typeof(string), expression);
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+            return double.Parse((string)row["expression"]);
+        }
+
+        private static void ValidateExpression(string expression)
+        {
+           
+            string validChars = "0123456789.+-*/";
+            if (expression.Any(c => !validChars.Contains(c)))
+            {
+                throw new Exception("Invalid characters in expression");
+            }
+
+       
+            string[] operators = { "+", "-", "*", "/" };
+            if (expression.Split(operators, StringSplitOptions.RemoveEmptyEntries).Length == 0)
+            {
+                throw new Exception("No valid operators in expression");
+            }
+
+
+            if (!IsExpressionValid(expression))
+            {
+                throw new Exception("Invalid expression format");
+            }
+        }
+
+        private static bool IsExpressionValid(string expression)
+        {
+        
+            string[] operators = { "+", "-", "*", "/" };
+            string[] tokens = expression.Split(operators, StringSplitOptions.RemoveEmptyEntries);
+            if (tokens.Length < 2)
+            {
+                return false;
+            }
+
+       
+            for (int i = 0; i < tokens.Length - 1; i++)
+            {
+                if (!IsNumber(tokens[i]) || !operators.Contains(expression[expression.IndexOf(tokens[i]) + tokens[i].Length].ToString()))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsNumber(string token)
+        {
+            double number;
+            return double.TryParse(token, out number);
+        }
+    }
+}
